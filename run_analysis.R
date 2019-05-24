15f3b5694b4c8af7263ea0d9fe5ee9dcb2198f57
Michael Walker <walker.mwalker9@gmail.com>
2019-05-23 20:32:14 -0600

    done through part 4

--- run_analysis.R
+++ run_analysis.R
@@ -12,3 +12,20 @@ colnames(totalDataSet) <- c(as.character(featureNames[,2]), 'activityLabel')
 #part 2, extract only means/standard deviations for each measurement
 columnsToKeep <- which(grepl('std\\(\\)', colnames(totalDataSet)) | grepl('mean\\(\\)', colnames(totalDataSet)))
 prunedDataSet <- totalDataSet[,c(columnsToKeep, 562)]
+
+#part 3, Descriptive Activity Names
+labels <- read.table('./Dataset/activity_labels.txt', col.names = c('id', 'activity_name'))
+prunedDataSet$activityLabel <- factor(prunedDataSet$activityLabel)
+levels(prunedDataSet$activityLabel) <- labels$activity_name
+prunedDataSet$activityLabel <- as.character(prunedDataSet$activityLabel)
+
+names(prunedDataSet) <- gsub('tBody', 'time-domain-body-', names(prunedDataSet))
+names(prunedDataSet) <- gsub('Acc', 'accelerometer', names(prunedDataSet))
+names(prunedDataSet) <- gsub('mean\\(\\)', 'average', names(prunedDataSet))
+names(prunedDataSet) <- gsub('std\\(\\)', 'st-deviation', names(prunedDataSet))
+names(prunedDataSet) <- gsub('tGravity', 'time-domain-gravity-', names(prunedDataSet))
+names(prunedDataSet) <- gsub('Mag', '-magnitude', names(prunedDataSet))
+names(prunedDataSet) <- gsub('fBody', 'frequency-domain-body-', names(prunedDataSet))
+names(prunedDataSet) <- gsub('Gyro', 'gyroscope', names(prunedDataSet))
+
+
